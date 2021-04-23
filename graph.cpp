961946a4868cf7ac7362c90b751a30ee858bb8b0
jgcortez <79665154+jgcortez@users.noreply.github.com>
2021-04-22 18:20:36 -0700

    Fixed Segmentation Fault/Print Format

--- graph.cpp
+++ graph.cpp
@@ -78,6 +78,8 @@ int dijkstra(int n, pNODE *A, int s, int t, int flag){
     element->vertex = s;
     element->key = 0;
     Insert(heap, element);
+    //printf("First insert\n");
+//printf("flag=%d\n", flag);
     if (flag == 1){
         // print insertion information
         printf("Insert vertex %d, key=%12.4f\n", element->vertex, element->key);
@@ -85,23 +87,29 @@ int dijkstra(int n, pNODE *A, int s, int t, int flag){
 
     while (heap->size){
         element = DeleteMin(heap, &flag, &count_Heapify);
-        if (flag == 1){
+        //printf("DeleteMin\n");
+	if (flag == 1){
             // print deletion information
             printf("Delete vertex %d, key=%12.4f\n", element->vertex, element->key);
         }
         u = element->vertex;
         V[u].color = 2;
-        if (element->vertex == t){
-            break;
+//printf("fault1\n");
+        if (element->vertex == t){  //if u == t? and return 0?
+            //return 0;
+	    break;
+//printf("in\n");
         }
+//printf("vertex=%d, t=%d\n", element->vertex, t);
+//printf("size=%d\n", heap->size);
         free(element);
 
-        node =A[u];
-        while (node){
+        node = A[u];
+        while (node){       // for each v in u.adj
             v = node->v;
             w = node->w;
-            if (V[v].color == 0){
-                V[v].dist = V[v].dist + w;
+            if (V[v].color == 0){   // if color is white
+                V[v].dist = V[u].dist + w;  
                 V[v].pi = u;
                 V[v].color = 1;
                 // printf("V[%d].color to 1\n", v);
@@ -109,9 +117,36 @@ int dijkstra(int n, pNODE *A, int s, int t, int flag){
                 element = (ELEMENT *) malloc(sizeof(ELEMENT));
                 element->vertex = v;
                 element->key = V[v].dist;
-                // More code 
-            }
+                // My code ahead
+                Insert(heap, element);      //When do you free? Count to free later?
+                //printf("Insert\n");
+		if (flag == 1){
+			printf("Insert vertex %d, key=%12.4f\n", element->vertex, element->key);
+		}
+		// free(element);           // ??
+                // heapPrint(heap);
+            } else if (V[v].dist > V[u].dist + w){
+                if (flag == 1){
+			printf("Decrease key of vertex %d, from %12.4f to %12.4f\n", v, V[v].dist, V[u].dist+w);
+		}
+		V[v].dist = V[u].dist + w;  
+                V[v].pi = u;
+                pos = V[v].pos;
+		DecreaseKey(heap, pos, V[v].dist);       // do I pass V[v] instead? v?/
+        	//printf("DecreaseKey\n");  
+	 }
+            // How does node get updated for if? Insert?
+	    node = node->next;
+            // Stack?
+            //printf("next\n");
         }
+	//printf("next 2\n");
+	// free(element);
     }
-
+    if (heap->size == 0){ // or !node ? or !A ?
+        return 1;
+    } else {
+        return 0;
+    } 
+    // When is return 0 vs return 1? How?
 }
