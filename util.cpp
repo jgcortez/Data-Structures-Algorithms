00fff336732475876c2684fc66693249dd773d08
jgcortez <79665154+jgcortez@users.noreply.github.com>
2021-04-22 22:06:47 -0700

    Clean FinalVersion of util.cpp

--- util.cpp
+++ util.cpp
@@ -3,21 +3,16 @@
 #include "util.h"
 #include <ctype.h>
 
-// 00-Programming-Help/03-nextWord
-
 int nextWord(char *word){
     
     char c;
     int i, r_value;
 
-//---------------------------------------------------
-//  search for the start of the next word
-//---------------------------------------------------
-
+// --- Search start of next word ---
     r_value = 0;
     c = getchar();
 
-#if _util_version == 1
+#if _util_version == 1      // Notice #if
     while (c == ' ' || c == '\t' || c == '\n'){
         c = getchar();
     }
@@ -28,10 +23,7 @@ int nextWord(char *word){
     }
 #endif
 
-//---------------------------------------------------
-//  within a word
-//---------------------------------------------------
-
+// --- Within a word ---
     i = 0;
     while (c != EOF){
 #if _util_version == 1
@@ -48,12 +40,10 @@ int nextWord(char *word){
         i++;
         c = getchar();
     }
+    
+// --- End of a word ---
 
-//---------------------------------------------------
-//  end of a word
-//---------------------------------------------------
-
-    word[i] = '\0';
+    word[i] = '\0';     // Notice '\0' instead of "\0"
 
     return r_value;
 }
